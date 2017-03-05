29d5026e93c41c678505d6dfb4eba7529973e4a0
Raja Khan <rajakhan@etech.local>
2017-03-05 03:59:17 +0000

    Fixed up UI for Recruitment

--- web/src/views/Main/Authenticated/Recruitment/Recruitment.js
+++ web/src/views/Main/Authenticated/Recruitment/Recruitment.js
@@ -29,20 +29,20 @@ export class Recruitment extends React.Component {
   createNew = () => {
 
     fetch('http://localhost:8080/applicants/update/', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ name: this.state.newName })
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name: this.state.newName })
     })
-    .then(() => {
-       console.log("Done")
-       this.reload()
-     })
-    .catch((err) => {
+      .then(() => {
+        console.log("Done")
+        this.reload()
+      })
+      .catch((err) => {
         console.log('broken!');
-    })
+      })
   }
 
   clicked = (e) => {
@@ -56,18 +56,20 @@ export class Recruitment extends React.Component {
     return (
       <div>
         <h2>Recruitment</h2>
-
-        <form>
-          <div className="form-group">
-            <input type="text" valueLink={linkState(this, 'newName')} />
-
-            <button type="button" onClick={this.createNew} className="btn btn-primary">Create New</button>
-          </div>
-
-
-        </form>
-
         <div className="container-fluid">
+          <form>
+            <div className="form-group">
+              <div className="row">
+                <div className="col-md-4">
+                  <input type="text" valueLink={linkState(this, 'newName')} className="form-control" />
+                </div>
+                <div className="col-md-6">
+                  <button type="button" onClick={this.createNew} className="btn btn-success">Create New</button>
+                </div>
+              </div>
+            </div>
+          </form>
+
           <table className="table table-bordered table-hover">
             <thead>
               <tr>
@@ -94,7 +96,7 @@ export class Recruitment extends React.Component {
 }
 
 Recruitment.contextTypes = {
-    router: React.PropTypes.object
+  router: React.PropTypes.object
 }
 
 export default Recruitment
